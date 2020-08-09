6dad585bda35d7e131cc56c5c358d2201bbaecb8
Sagar Devkota <68952589+tikapuresagar@users.noreply.github.com>
2020-08-10 01:01:30 +0545

    Update script.js

--- mod4_soln/script.js
+++ mod4_soln/script.js
@@ -34,8 +34,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
-  var byeSpeaker[];
-  var helloSpeaker[];
+
 
   if (firstLetter === 'j') {
     byeSpeaker(names[i]);
